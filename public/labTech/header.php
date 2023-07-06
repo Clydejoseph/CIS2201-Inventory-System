df5045d76281fac96354bd31d98f71e5f7f180a7
Clyde <noobclyde21@gmail.com>
2023-07-06 20:28:25 +0800

    initial project

---
+++ public/labTech/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="/inventory/css/header-style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="dashboard.php">Inventory System</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mr-auto"></ul>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">ASSET/TOOL</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">REQUEST FORMS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">REPORT/ANALYTICS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">FEEDBACKS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">LOG OUT</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
