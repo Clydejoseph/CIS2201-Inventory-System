ec0dc9c8f0f29f1759365183b3e6510c222da2ef
Jolo Tangpuz <121079458+b1gmcdaddy@users.noreply.github.com>
2023-07-16 23:31:41 +0800

    Update Homepage.js

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -20,7 +20,7 @@ import TechNav from './component/navBars/Nav-LabTech';
 
 import HeadAsset from './pages/labHead/Head-Asset';
 import HeadRequest from './pages/labHead/Head-Request';
-import HeadLogs from './pages/labHead/Head-Logs';
+import HeadLog from './pages/labHead/Head-Logs';
 
 import AdminAccount from './pages/admin/Admin-Account';
 import AdminAsset from './pages/admin/Admin-Asset';
