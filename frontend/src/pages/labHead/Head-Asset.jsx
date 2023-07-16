64f373f141fe958247be089c2de69df512cf29b5
Jolo Tangpuz <121079458+b1gmcdaddy@users.noreply.github.com>
2023-07-16 23:29:29 +0800

    Update Head-Asset.jsx

--- frontend/src/pages/labHead/Head-Asset.jsx
+++ frontend/src/pages/labHead/Head-Asset.jsx
@@ -15,55 +15,40 @@ import {
   import '../../css/table-asset.css'
 
 export default function HeadAsset(){
-    return(
-
-        <>
-            <div className='table'>
-
-            <div id='asset-header'>
-                <Heading size='xl'>List of Items:</Heading>
-            </div>
-
-            <TableContainer>
-                <Table className='asset-table' variant='simple'>
-
-                    {/* <TableCaption>Imperial to metric conversion factors</TableCaption> */}
-
-                    <Thead className='asset-head'>
-                    <Tr>
-                        <Th>Asset Code</Th>
-                        <Th>Name</Th>
-                        <Th>Brand</Th>
-                        <Th>Serial No.</Th>
-                        <Th>Date Aquired</Th>
-                        <Th>Location</Th>
-                        <Th>Status</Th>
-                    </Tr>
-                    </Thead>
-                    <Tbody>
-                    <Tr>
-                        <Td>aa</Td>
-                        <Td>a</Td>
-                        <Td>a</Td>
-                        <Td>a</Td>
-                        <Td>aa</Td>
-                        <Td>a</Td>
-                        <Td>a</Td>
-                    </Tr>
-                    <Tr>
-                        <Td>aa</Td>
-                        <Td>a</Td>
-                        <Td>a</Td>
-                        <Td>a</Td>
-                        <Td>aa</Td>
-                        <Td>a</Td>
-                        <Td>a</Td>
-                    </Tr>
-                    </Tbody>
-                </Table>
+    return (
+        <div className="tech-asset">
+              <Heading size="xl" color={"black"} fontFamily={"rubik"}>
+                Equipment
+              </Heading>
+            <TableContainer
+              borderRadius={"10px"}
+              width={"100%"}
+              overflowY={"auto"}
+              boxShadow={"xl"}
+              height={"70vh"}
+            >
+              <Table colorScheme={"facebook"} variant="simple">
+                <Thead>
+                  <Tr
+                    position={"sticky"}
+                    top={0}
+                    bgColor={"facebook.400"}
+                    zIndex={"1"}
+                  >
+                    <Th color={"white"}>Asset Code</Th>
+                    <Th color={"white"}>Name</Th>
+                    <Th color={"white"}>Brand</Th>
+                    <Th color={"white"}>Serial No.</Th>
+                    <Th color={"white"}>Date Aquired</Th>
+                    <Th color={"white"}>Location</Th>
+                    <Th color={"white"}>Status</Th>
+                    <Th color={"white"}></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                </Tbody>
+              </Table>
             </TableContainer>
-
-            </div>
-        </>
-    )
-}
+        </div>
+      );
+    }
