e6fca5e148ca5237b92684f9e3a91cb41ac472b7
Jolo Tangpuz <121079458+b1gmcdaddy@users.noreply.github.com>
2023-07-16 20:56:35 +0800

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,99 +1,130 @@
-import {React , useState } from 'react'
-import {useNavigate} from 'react-router-dom'
-import './css/Login.css'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './css/Login.css';
 
 //data
-import { LoginAccounts } from './dataExport/Loginaccount'
+import { LoginAccounts } from './dataExport/Loginaccount';
 
-import { Card, CardHeader, Heading,Input,Button, CardBody, Stack } from '@chakra-ui/react'
+import {
+  Card,
+  CardHeader,
+  Heading,
+  Input,
+  Image,
+  Button,
+  CardBody,
+  Stack,
+} from '@chakra-ui/react';
 import {
   FormControl,
   FormErrorMessage,
-} from '@chakra-ui/react'
-
+} from '@chakra-ui/react';
 
 function Login() {
-
   const linkto = useNavigate();
 
-  const [username , setUname] = useState('');
-  const [password , setPword] = useState('');
-  
-  const [Uerror , setU_Error] = useState(false);
-  const [Perror , setP_Error] = useState(false);
-  //if login clicked
-  
+  const [username, setUname] = useState('');
+  const [password, setPword] = useState('');
 
-  const HandlePword = (e) =>{
+  const [Uerror, setU_Error] = useState(false);
+  const [Perror, setP_Error] = useState(false);
+
+  // Handle password input
+  const HandlePword = (e) => {
     setPword(e.target.value);
     setP_Error(false);
-
   };
-  
-    const HandleUname = (e) =>{
-      setUname(e.target.value);
-      setU_Error(false);
-    };
-  
-  function submitForm(e){
-    
+
+  // Handle username input
+  const HandleUname = (e) => {
+    setUname(e.target.value);
+    setU_Error(false);
+  };
+
+  // Form submission
+  function submitForm(e) {
     e.preventDefault();
-    if(username === ''){setU_Error(true)};
-    if(password === ''){setP_Error(true)};
 
-    if(username !== '' && password !== ''){
+    if (username === '') {
+      setU_Error(true);
+    }
+    if (password === '') {
+      setP_Error(true);
+    }
 
+    if (username !== '' && password !== '') {
+      let result = LoginAccounts.find((res) => res.username === username);
 
-      let result = LoginAccounts.find(res => res.username === username);
-      
-      if(result){
+      if (result) {
         console.log(result);
-        sessionStorage.setItem('account' , JSON.stringify({username:result.username , password:result.password , auth: result.authority}));
+        sessionStorage.setItem(
+          'account',
+          JSON.stringify({
+            username: result.username,
+            password: result.password,
+            auth: result.authority,
+          })
+        );
         linkto('/');
-      }
-      else{
+      } else {
         setU_Error(true);
       }
-      
-      // sessionStorage.setItem('account' , JSON.stringify({username:username , password:password}))
-      // navigatePage('/');
-
-      
     }
-    
-
-  };
-
+  }
 
   return (
-        <div className="Login">
-          <Card align='center' className='cards'>
-            <CardHeader>
-              <Heading size='md'>Log in</Heading>
-            </CardHeader>
-            <CardBody>
-            <FormControl as='fieldset' className='LoginBox'>
-                  <Stack spacing={4} direction='column'>
-                  <Stack spacing={4} direction='column'>
-                    <FormControl isInvalid={Uerror}>
-                    {!Uerror ? ("") : (<FormErrorMessage>Invalid username </FormErrorMessage>) }
-                      <Input type="text" placeholder={'Username'} value={username} onChange={HandleUname}/>
-                    </FormControl>
-                    <FormControl isInvalid={Perror}>
-                      {!Perror ? ("") : (<FormErrorMessage>Invalid password </FormErrorMessage>) }
-                      <Input type="password" placeholder='Password' value={password} onChange={HandlePword}/>
-                    </FormControl>
-                  </Stack>
-                    <Button colorScheme='messenger' 
-                            onClick={submitForm}  
-                            loadingText='Submitting'
-                            >Log in</Button>
-                  </Stack>
-            </FormControl>
-            </CardBody>
-          </Card>
-        </div>
-  )
+    <div className="Login">
+      <Card align="center" className="cards">
+        <img src="/DCISM.png" alt="Logo" className="Logo" />
+
+        <CardHeader>
+          <Heading size="md" className='header'>LOGIN</Heading>
+        </CardHeader>
+        <CardBody>
+          <FormControl as="fieldset" className="LoginBox">
+            <Stack spacing={4} direction="column">
+              <Stack spacing={4} direction="column">
+                <FormControl isInvalid={Uerror}>
+                  {!Uerror ? (
+                    ''
+                  ) : (
+                    <FormErrorMessage>Invalid username </FormErrorMessage>
+                  )}
+                  <Input
+                    type="text"
+                    placeholder={'Username'}
+                    value={username}
+                    onChange={HandleUname}
+                  />
+                </FormControl>
+                <FormControl isInvalid={Perror}>
+                  {!Perror ? (
+                    ''
+                  ) : (
+                    <FormErrorMessage>Invalid password </FormErrorMessage>
+                  )}
+                  <Input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={HandlePword}
+                  />
+                </FormControl>
+              </Stack>
+              <Button
+                colorScheme="messenger"
+                onClick={submitForm}
+                loadingText="Submitting"
+                disabled={!username || !password}
+              >
+                Log in
+              </Button>
+            </Stack>
+          </FormControl>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
