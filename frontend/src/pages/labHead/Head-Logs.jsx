c9c6bc2f27f475841a3fe43b154837d8feba1114
Jolo Tangpuz <121079458+b1gmcdaddy@users.noreply.github.com>
2023-07-16 23:32:15 +0800

    Update Head-Logs.jsx

--- frontend/src/pages/labHead/Head-Logs.jsx
+++ frontend/src/pages/labHead/Head-Logs.jsx
@@ -1,53 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-    Heading, Button
-  } from '@chakra-ui/react'
-
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Heading,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
+  useDisclosure,
+  VStack,
+  HStack,
+  Divider,
+  Textarea,
+} from "@chakra-ui/react";
   import '../../css/table-asset.css'
 
 export default function HeadLog(){
     return(
 
         <>
-            <div className='table'>
 
-            <div id='asset-header'>
-                <Heading size='lg'>Recent Activity</Heading>
-            </div>
+            <div className='table-request'>
 
-            <TableContainer>
-                <Table className='asset-table' variant='simple'>
 
+            <VStack>
+            <HStack justify={'space-between'} width={'100%'} padding={'0px 8px 0px 8px'}>
+              <Heading size='xl' color={'black'} fontFamily={'rubik'}>Activity Log</Heading>
+              <HStack>
+              </HStack>
+            </HStack>
+
+
+            <TableContainer borderRadius={'10px'} width={'100%'} overflowY={'auto'} boxShadow={'xl'} height={'70vh'} >
+                <Table colorScheme='facebook'  variant='simple' >
                     {/* <TableCaption>Imperial to metric conversion factors</TableCaption> */}
 
-                    <Thead className='asset-head'>
-                    <Tr>
-                        <Th>Date</Th>
-                        <Th>Name</Th>
-                        <Th id='a-h'>Activity</Th>
-                        <Th></Th>
+                    <Thead>
+                    <Tr position={'sticky'} top={0} bgColor={'facebook.400'} zIndex={'1'}>
+                        <Th color={'white'}>Date</Th>
+                        <Th color={'white'}>User</Th>
+                        <Th color={'white'}>Activity</Th>
+                        <Th color={'white'}>Details</Th>
                     </Tr>
-                    </Thead>
-                    <Tbody>
                     <Tr>
-                        <Td>aa</Td>
-                        <Td>a</Td>
-                        <Td id='activity'>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</Td>
+                        <Td></Td>
+                        <Td></Td>
+                        <Td></Td>
                         <Td><Button>Details</Button></Td>
                     </Tr>
-                    </Tbody>
+                    </Thead>
                 </Table>
             </TableContainer>
-
+            </VStack>
             </div>
         </>
     )
